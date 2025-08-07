02c37d28cb177bf6c174ce238a9e6fe6700836a0
rattantv <rattantv@ais.co.th>
2025-08-07 09:52:13 +0700

    try again ci

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const WebpackAssetsManifest = require('webpack-assets-manifest');
 
 module.exports = {
     reactStrictMode: true,
+     distDir: 'build', 
     async headers() {
         return [
             {
